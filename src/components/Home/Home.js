12fb1d067ee5db203198f3682e76d4b43541879f
Syed Mohammad Younis <younushussain0@gmail.com>
2024-04-07 04:06:48 +0500

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -13,14 +13,26 @@ const Test = () => {
       }}
       className="container"
     >
-      <div style={{ position: "relative", width: "60%", fontWeight: "400" }}>
-        <h4 style={{ textAlign: "start", fontSize: "40px", marginBottom: "0" }}>
-          Documented into <br />
-          200 pages <span style={{ fontStyle: "italic" }}>report of you</span>
+      <div
+        style={{ position: "relative", width: "60%", fontWeight: "400" }}
+        className="text_content"
+      >
+        <h4
+          style={{ textAlign: "start", fontSize: "40px", marginBottom: "0" }}
+          className="heading"
+        >
+          Documented <br className="h_br_1" /> into <br className="h_br_2" />
+          200 pages{" "}
+          <span style={{ fontStyle: "italic", fontFamily: "NeueItalic" }}>
+            report of you
+          </span>
         </h4>
-        <p style={{ textAlign: "start", fontSize: "24px", marginTop: "10px" }}>
-          Professional facial assessments and clear facial <br /> changes
-          visualisation.
+        <p
+          style={{ textAlign: "start", fontSize: "24px", marginTop: "10px" }}
+          className="pera"
+        >
+          Professional facial assessments and clear facial{" "}
+          <br className="p_br_1" /> changes visualisation.
         </p>
 
         <button
@@ -34,12 +46,30 @@ const Test = () => {
             outline: "none",
             border: "none",
           }}
+          className="btn_1"
         >
           ALL REPORTS
         </button>
       </div>
 
       <ImageAnimation />
+      <button
+        style={{
+          position: "absolute",
+          left: "0",
+          backgroundColor: "#809AA3",
+          width: "86%",
+          padding: "1rem",
+          color: "#FFFFFF",
+          outline: "none",
+          border: "none",
+          bottom: "-10rem",
+          transform: " translate(20px, 10px)",
+        }}
+        className="btn_2"
+      >
+        ALL REPORTS
+      </button>
     </div>
   );
 };
