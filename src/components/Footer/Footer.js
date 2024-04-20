b836b09844ebe9fd1294c924d1a8bd0c32bf0c1c
Syed Mohammad Younis <younushussain0@gmail.com>
2024-04-20 13:47:04 +0500

    add footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Footer.css";
+const Footer = () => {
+  return (
+    <div className="footer">
+      Developed By :{" "}
+      <a href="https://www.upwork.com/freelancers/~0193a5a094c565b3a7" target="_blank">
+        {" "}
+        Ali Ahmed
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
