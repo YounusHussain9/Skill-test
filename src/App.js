e9ff7f0b8c4a6e82f4ba02fb2a30e5d30afe1d10
Syed Mohammad Younis <younushussain0@gmail.com>
2024-04-20 13:48:29 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import Footer from "./components/Footer/Footer";
 import Navbar from "./components/navbar/navbar";
 import ScrollContent from "./components/ScrollContent/ScrollContent";
 
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Navbar />
       <ScrollContent />
+      <Footer />
     </div>
   );
 }
